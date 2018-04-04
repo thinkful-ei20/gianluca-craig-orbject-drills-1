21270b6c331e2d9b5d9f5eee4d6c8a87d2783d93
Craig Pounds <nweaver@windstream.net>
2018-04-04 11:01:17 -0500

    added personMaker()

--- object-drills-1.js
+++ object-drills-1.js
@@ -1,14 +1,16 @@
+'use strict';
+
 //Object creator
 
 function createMyObject() {
-	return {
-		foo: 'bar',
-		answerToUniverse: 42,
-		'olly olly': 'oxen free',
-		sayHello: function() {
-			return 'hello';
-		}
-	};
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+      return 'hello';
+    }
+  };
 }
 
 //console.log(createMyObject());
@@ -16,15 +18,28 @@ function createMyObject() {
 //Object Updater
 
 function updateObject(obj) {
-	obj.foo = 'foo';
-	obj.bar = 'bar';
-	obj.bizz = 'bizz';
-	obj.bang = 'bang';
-	return obj;
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
 }
 
 let input = {};
 
 updateObject(input);
 
-console.log(input);
+//console.log(input);
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+  return person;
+}
