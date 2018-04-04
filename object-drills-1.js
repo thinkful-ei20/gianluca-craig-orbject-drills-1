ff0013b9973b219d918ddc0f33fcc07c7e74ddf3
Craig Pounds <nweaver@windstream.net>
2018-04-04 11:06:25 -0500

    added keyDeleter()

--- object-drills-1.js
+++ object-drills-1.js
@@ -43,3 +43,16 @@ function personMaker() {
   };
   return person;
 }
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+var sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
